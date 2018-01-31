346d6dfb306853129526640214b31061d4e2fb70
Greyson Wright <greysonmwright@gmail.com>
2018-01-31 17:50:17 -0600

    - reads in commands

---
+++ Assign1B/Assign1B/assign1b.c
@@ -0,0 +1,48 @@
+//
+//  main.c
+//  Assign1B
+//
+//  Created by Greyson Wright on 1/31/18.
+//  Copyright © 2018 Greyson Wright. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX_LINE 80 /* The maximum length command */
+
+char *readToken(char *);
+
+int main(void) {
+	char *args[MAX_LINE / 2 + 1];
+	int should_run = 1;
+	
+	while (should_run) {
+		printf("osh>");
+		fflush(stdout);
+		
+		char *str = malloc(MAX_LINE + 1);
+		fgets(str, MAX_LINE, stdin);
+		
+		char *token = strtok(str, " \n");
+		int count = 0;
+		while (token) {
+			args[count++] = token;
+			token = strtok(0, " \n");
+		}
+		
+		if (!strcmp(args[0], "exit")) {
+			should_run = 0;
+		}
+		/**
+		 * After reading user input, the steps are:
+		 * (1) fork a child process using fork()
+		 * (2) the child process will invoke execvp()
+		 * (3) if command included &, parent will invoke wait() */
+	}
+	return 0;
+}
+
+
