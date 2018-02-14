3f8f4342177c4391ecc71c284635a5221934ec48
Greyson Wright <greysonmwright@gmail.com>
2018-02-14 17:53:28 -0600

    - hopefully finished assign2

--- Assign2/420/420/main.c
+++ Assign2/420/420/main.c
@@ -20,6 +20,9 @@ pthread_mutex_t mut;
 PIDManager *pidManager;
 
 int allocate_map(void);
+void createThreads(pthread_t *);
+void joinThreads(pthread_t *);
+void *testManager(void *);
 int allocate_pid(void);
 int getMap(void);
 void release_pid(int);
@@ -27,16 +30,62 @@ void release_pid(int);
 int count;
 
 int main(int argc, const char **argv) {
-	pthread_mutex_init(<#pthread_mutex_t *restrict _Nonnull#>, <#const pthread_mutexattr_t *restrict _Nullable#>)
-	allocate_map();
+	pthread_t *tids = malloc((sizeof *tids) * 100);
+	int didAllocMap = allocate_map();
+	pthread_mutex_init(&mut, 0);
+	srand((unsigned) time(0));
 	
+	if (!didAllocMap) {
+		exit(-1);
+	}
+	
+	createThreads(tids);
+	joinThreads(tids);
 	return 0;
 }
 
 int allocate_map() {
 	pidManager = newPIDManager(MAX_MAP);
-	
+	if (pidManager == 0) {
+		printf("Could not allocate map.\n");
+		return -1;
+	}
 	return 1;
+}
+
+void createThreads(pthread_t *tids) {
+	for (int i = 0; i < 100; i++) {
+		pthread_create(&tids[i], 0, testManager, 0);
+	}
+}
+
+void joinThreads(pthread_t *tids) {
+	for (int i = 0; i < 100; i++) {
+		pthread_join(tids[i], 0);
+	}
+}
+
+void *testManager(void *args) {
+	pthread_mutex_lock(&mut);
+	int pid = allocate_pid();
+	pthread_mutex_unlock(&mut);
+	
+	if (pid == -1) {
+		printf("Could not allocate pid.\n");
+		pthread_exit(0);
+	}
+	
+	printf("allocated %d\n",pid);
+	
+	sleep(rand() % 5);
+	
+	pthread_mutex_lock(&mut);
+	release_pid(pid);
+	pthread_mutex_unlock(&mut);
+	
+	printf("released %d\n",pid);
+	
+	pthread_exit(0);
 }
 
 int allocate_pid() {
